ee9291d9cf5434451dca87f1481baeaeb2eae3db
rishabhm127 <95129392+rishabhm127@users.noreply.github.com>
2021-11-28 13:34:32 +0530

    width and hiegt logo final

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -63,7 +63,7 @@ const Header = ({ t }: any) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="160px" height="110px" />
+            <SvgIcon src="logo.svg" width="350px" height="150px" />
           </LogoContainer>
           <NotHidden>
             <MenuItem />
